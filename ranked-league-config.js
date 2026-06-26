b387891c4f0c375faa9e1fd86199dd334a9a0e99
Aidan Kellaher <nextweekmedia@gmail.com>
2026-06-26 13:24:28 -0500

    Update ranked-league-config.js

--- ranked-league-config.js
+++ ranked-league-config.js
@@ -3,7 +3,7 @@ export const RANKED_LEAGUE_WORKER_URL = "https://rapid-haze-012c.nextweekmedia.w
 export const RANKED_LEAGUE_TEAMUP_URL = "https://teamupgg.com/leaderboard/DISCORD%7C1069003073311211601";
 
 export const SHOTGUN_PRO_LEAGUE_DEFAULT_SEASON = 7;
-export const SHOTGUN_PRO_LEAGUE_DEFAULT_STAGE = 2;
+export const SHOTGUN_PRO_LEAGUE_DEFAULT_STAGE = 3;
 export const SHOTGUN_PRO_LEAGUE_MAX_SEASON_TO_CHECK = 7;
 
 export const SUPER_LEAGUE_SEASON = "Season 6";
