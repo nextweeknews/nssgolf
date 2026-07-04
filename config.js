8fa9b208d4b1c62e08572fe18a4cfb1f5c67b510
Aidan Kellaher <nextweekmedia@gmail.com>
2026-07-04 13:59:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const CURRENT_RANKED_LEAGUE_SEASON = "Season_12";
+export const CURRENT_RANKED_LEAGUE_SEASON = "Season_13";
 export const RANKED_LEAGUE_WORKER_URL = "https://rapid-haze-012c.nextweekmedia.workers.dev";
 export const RANKED_LEAGUE_TEAMUP_URL = "https://teamupgg.com/leaderboard/DISCORD%7C1069003073311211601";
 
